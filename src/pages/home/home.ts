b8fdada732b17f962c51e88aae13baddb9cf1358
ABONO <bonoadil@gmail.com>
2017-03-27 22:21:55 +0200

    initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild } from '@angular/core';
+
+import { NavController, Content } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  @ViewChild(Content) content: Content;
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+    //Util
+  scrollToBottom() {
+      setTimeout(() => {
+        this.content.scrollToBottom();
+      });
+  }
+
+}
